c9f87186d329432bfb8fe674babd70249ca70ce7
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-12-25 17:01:29 +0100

    Set KikiriCoin as capped and allow minting only up to 10 tokens each time

--- contracts/KikiriCoin.sol
+++ contracts/KikiriCoin.sol
@@ -3,12 +3,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KikiriCoin is ERC20, Ownable {
-    constructor() ERC20("KikiriCoin", "KIKI") {}
+contract KikiriCoin is ERC20Capped, Ownable {
+    constructor(uint256 cap) ERC20("KikiriCoin", "KIKI") ERC20Capped(cap) {}
 
     function issueToken(uint256 amount) public onlyOwner{
-        _mint(msg.sender, amount*10**18);
+        require(amount <= 10*10**18, "Amount can't be higher than 10 KIKI");
+        _mint(msg.sender, amount);
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,81 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { fail } from 'assert';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+// eslint-disable-next-line node/no-missing-import
+import { KikiriCoin } from '../typechain';
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+const produceStringOfZeros = (length: number) =>
+  Array(length)
+    .fill('0')
+    .reduce((m, i) => `${m}${i}`, '');
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+const DECIMALS = 18;
+const CAP = `1000000${produceStringOfZeros(DECIMALS)}`;
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+const toDecimalString = (amount: number) => {
+  const integer = Math.floor(amount);
+  const decimals = amount - integer;
+  const decimalsString = `${decimals}`.replace('0.', '');
+  return `${integer}${decimalsString}${produceStringOfZeros(DECIMALS - decimalsString.length)}`;
+};
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+describe('KikiriCoin', () => {
+  const deployContract = async (cap = CAP): Promise<KikiriCoin> => {
+    const KikiriCoin = await ethers.getContractFactory('KikiriCoin');
+    const kiririCoin = await KikiriCoin.deploy(cap);
+    await kiririCoin.deployed();
+    return kiririCoin;
+  };
+
+  describe('issueToken', () => {
+    it('Should issue token to the owner', async () => {
+      const kiririCoin = await deployContract();
+      expect(await kiririCoin.totalSupply()).to.equal(0);
+
+      const issueTokenTx = await kiririCoin.issueToken(toDecimalString(1));
+      await issueTokenTx.wait();
+
+      expect(await kiririCoin.totalSupply()).to.equal('1000000000000000000');
+    });
+
+    it('Should not issue a negative amount of token', async () => {
+      const kiririCoin = await deployContract();
+      try {
+        await kiririCoin.issueToken(`-${toDecimalString(1)}`);
+      } catch (error) {
+        return;
+      }
+      fail('An error should have been thrown');
+    });
+
+    it('Should not issue token to another account', async () => {
+      const kiririCoin = await deployContract();
+      const [, ...addr] = await ethers.getSigners();
+      await expect(kiririCoin.connect(addr[0]).issueToken(toDecimalString(1))).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      );
+    });
+
+    it('Should not allow issuing more than 10 tokens', async () => {
+      const kiririCoin = await deployContract();
+      await expect(kiririCoin.issueToken(toDecimalString(11))).to.be.revertedWith(
+        "Amount can't be higher than 10 KIKI"
+      );
+    });
+
+    it('Should not allow issuing more than the cap', async () => {
+      const smallerCap = `15${produceStringOfZeros(DECIMALS)}`;
+      const kiririCoinSmallerCap = await deployContract(smallerCap);
+
+      expect(await kiririCoinSmallerCap.cap()).to.equal('15000000000000000000');
+
+      const issueTokenTx = await kiririCoinSmallerCap.issueToken(toDecimalString(10));
+      await issueTokenTx.wait();
+
+      await expect(kiririCoinSmallerCap.issueToken(toDecimalString(10))).to.be.revertedWith(
+        'ERC20Capped: cap exceeded'
+      );
+    });
   });
 });
