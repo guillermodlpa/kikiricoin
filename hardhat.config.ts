c5be1780185b23718fbcaef28f9b90b3fd3d624f
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-12-26 16:04:06 +0100

    Remove default comment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,9 +11,6 @@ import './tasks.config';
 
 dotenv.config();
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   solidity: '0.8.4',
   networks: {
