a10bbe32320875899f0014d0b2b3dbe1a577d6ad
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-12-26 11:08:34 +0100

    Fix ESLint config to not report error on TypeScript imports from tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,19 @@ module.exports = {
     mocha: true,
     node: true,
   },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
+  extends: ['standard', 'plugin:prettier/recommended', 'plugin:node/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
+    'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
+  },
+  // For .ts file imports, see https://stackoverflow.com/a/61024769
+  settings: {
+    node: {
+      tryExtensions: ['.js', '.json', '.node', '.ts', '.d.ts'],
+    },
   },
 };
