950a6f85c8403d33490948fc6959693261091781
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-12-27 17:12:51 +0100

    Comment out ropsten config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,10 +14,10 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: '0.8.4',
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || '',
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    // ropsten: {
+    //   url: process.env.ROPSTEN_URL || '',
+    //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // },
     mumbai: {
       url: '',
     },
