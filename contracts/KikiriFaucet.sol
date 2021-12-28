81a77c92337e552a3fab7207804228dea20fb30e
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-12-28 18:53:48 +0100

    Add comments and update variable name

--- contracts/KikiriFaucet.sol
+++ contracts/KikiriFaucet.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Faucet to distribute KIKI tokens
 /// @author Guillermo de la Puente
-/// @notice This faucet enables any account to receive token, as long as it's funded.
+/// @notice This faucet enables any account to receive token, as long as it's funded
 contract KikiriFaucet is Ownable {
     event Claim(address indexed to, uint256 amount);
     event Withdraw(address indexed owner, address indexed recipient, uint256 amount);
@@ -15,10 +15,11 @@ contract KikiriFaucet is Ownable {
     uint16 public constant RATE_LIMIT_TIME = 5 minutes;
     uint8 public constant DRIP_AMOUNT = 10;
 
+    // immutable means it becomes part of the contract's implementation at deployment time
     IERC20Metadata public immutable token;
 
-    // Simple rate limiting
-    mapping(address => uint256) private nextRequestAt;
+    // Simple rate limiting per address
+    mapping(address => uint256) private nextClaimAt;
 
     constructor(address kikiriCoinAddress) {
         token = IERC20Metadata(kikiriCoinAddress);
@@ -27,17 +28,19 @@ contract KikiriFaucet is Ownable {
     /// @notice You can use this function to claim a fixed amount of tokens. Notice that it's rate limited
     function claim() external {
         require(token.balanceOf(address(this)) > 1, "FaucetError: Empty");
-        require(nextRequestAt[msg.sender] < block.timestamp, "FaucetError: Try again later");
+        require(nextClaimAt[msg.sender] < block.timestamp, "FaucetError: Try again later");
 
-        nextRequestAt[msg.sender] = block.timestamp + RATE_LIMIT_TIME;
+        // Update state before transfering.
+        // This practice prevents a potential reentrancy attack that bypasses the rate limiting.
+        nextClaimAt[msg.sender] = block.timestamp + RATE_LIMIT_TIME;
 
         uint256 amount = DRIP_AMOUNT * 10**token.decimals();
         token.transfer(msg.sender, amount);
         emit Claim(msg.sender, amount);
     }
 
-    /// @notice Withdraw KIKI token deposited on the faucet. Only the smart contract owner can execute.
-    /// @param recipient Address to withdraw token to.
+    /// @notice Withdraw KIKI token deposited on the faucet. Only the smart contract owner can execute
+    /// @param recipient Address to withdraw token to
     /// @param amount Quantity of token to withdraw
     function withdrawToken(address recipient, uint256 amount) external onlyOwner {
         require(token.balanceOf(address(this)) >= amount, "FaucetError: Insufficient funds");
